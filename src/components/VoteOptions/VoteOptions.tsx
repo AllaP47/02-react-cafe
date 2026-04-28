bf38dfb065d902a8ca2a1e6f6a54fa121723524e
AllaP47 <allapaschinska1@gmail.com>
2026-04-28 21:41:45 +0300

    add message

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -11,9 +11,9 @@ const VoteOptions = ({ onVote, onReset, total }: VoteOptionsProps) => {
 
   return (
     <div className={css.container}>
-     <button className={css.btn} onClick={() => onVote('good')}>Good</button>
-      <button className={css.btn} onClick={() => onVote('neutral')}>Neutral</button>
-      <button className={css.btn} onClick={() => onVote('bad')}>Bad</button>
+     <button className={css.button} onClick={() => onVote('good')}>Good</button>
+      <button className={css.button} onClick={() => onVote('neutral')}>Neutral</button>
+      <button className={css.button} onClick={() => onVote('bad')}>Bad</button>
      {total > 0 && (
   <button className={`${css.button} ${css.reset}`} onClick={onReset}>
     Reset
